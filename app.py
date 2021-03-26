46df99e01b6ec95dd33e85a4fb25770303323b4f
William Bell <wjbell97@gmail.com>
2021-03-25 21:37:18 -0400

    added actual desired behavior for the mini bot

--- app.py
+++ app.py
@@ -6,8 +6,38 @@ from urllib.request import Request, urlopen
 
 from flask import Flask, request
 
+from random import choice
+
 app = Flask(__name__)
 
+prefixes = [
+    "Play without me I have to",
+    "Sorry guys I can't I gotta",
+    "Maybe later first I have to",
+    "Hold up I just gotta"
+]
+
+reasons = [
+    "do my dad's taxes",
+    "move scrap metal",
+    "drive my sister to her friend's house",
+    "pick up my sister from a party",
+    "watch paint dry",
+    "take my mom to the stor",
+    "hold the light for my dad",
+    "do every dog related chore",
+    "go to Grenada for the third time today",
+    "eat in dinner in two hours",
+    "buy Gatorade for my dad"
+]
+
+
+def oi_huey(data):
+    if '@Huey' in data['text']:
+        if 'oi' in data['text'].lower():
+            return True
+
+    return False
 
 @app.route('/', methods=['POST'])
 def webhook():
@@ -15,8 +45,9 @@ def webhook():
 
     # We don't want to reply to ourselves!
     if data['name'] != 'Huey':
-        msg = '{}, you sent "{}".'.format(data['name'], data['text'])
-        send_message(msg)
+        if oi_huey(data):
+            message = choice(prefixes) + ' ' + choice(reasons)
+            send_message(message)
 
     return "ok", 200
 
