544d461a3013127b6f8f51dd033701709589ee37
gfunderburg <gavin.funderburg@gmail.com>
2021-03-29 19:27:59 -0400

    syntax error fix

--- services/dota_game_service.py
+++ services/dota_game_service.py
@@ -64,8 +64,8 @@ def make_last_game_state_args(match_data: dict) -> tuple:
     start_time = match_data['start_time']
 
     current_player_data = None
-    for entry in match_data['players']
-        if entry['account_id']
+    for entry in match_data['players']:
+        if entry['account_id']:
             if int(entry['account_id']) == PLAYER_ID or 120813182 or 106692261 or 112984717 or 95549436 or 133187493 or 55335864 or 126859835:
                 friends_deaths.append(FRIENDS_MAP[entry['account_id']])
                 friends_deaths.append(int(entry['deaths']))
