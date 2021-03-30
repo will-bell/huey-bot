d7c3eb0064fba5aba23c79ac2c4d804b1741aa23
gfunderburg <gavin.funderburg@gmail.com>
2021-03-30 15:34:07 -0400

    indent

--- services/dota_game_service.py
+++ services/dota_game_service.py
@@ -180,7 +180,7 @@ def generate_old_game_notification(last_game_state: LastGameState) -> str:
         if friends_deaths[x+1] > previous_value:
             previous_value = friends_deaths[x+1]
             most_deaths = friends_deaths[x] + previous_value
-    x = x+1
+        x = x+1
 
     print(most_deaths)
 
