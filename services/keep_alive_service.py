7fe3b6f9cd80b5ff6f2d3d3efaa50ff3af28d913
William Bell <wjbell97@gmail.com>
2021-03-29 23:06:01 -0400

    debugging lol

--- services/keep_alive_service.py
+++ services/keep_alive_service.py
@@ -1,14 +1,18 @@
-from multiprocessing import Value
 import time
+from multiprocessing import Value
+
 import requests
 
+from common import send_message
 
 def keep_alive(loop: Value):
     url = 'https://huey-bot.herokuapp.com/keep_alive'
     
     last_time = time.time()
     while loop.value:
-        if time.time() - last_time > 60:
+        if time.time() - last_time > 10:
             res = requests.post(url=url)
             # print('ping')
             last_time = time.time()
+
+            send_message("I'm still alive")
