4e98b8e4fa78e9943dbf25c60088b774d5e4bc67
gfunderburg <gavin.funderburg@gmail.com>
2021-04-02 12:49:38 -0500

    added new prefixes and edited a couple

--- interaction/text_phrases.py
+++ interaction/text_phrases.py
@@ -23,10 +23,14 @@ greetings = [
 
 negatives = [
     "Be on in 40 I gotta",
-    "Play without me I have to",
-    "Sorry guys I can't I gotta",
-    "Maybe later first I have to",
-    "Hold up I just gotta"
+    "Play without me, I have to",
+    "Sorry guys I can't, I gotta",
+    "Maybe later, first I have to",
+    "Hold up I just gotta",
+    "20 mins, gotta",
+    "Play one -",
+    "Go ahead and play one, I need to",
+    "Not tonight, need to"
 ]
 
 reasons = [
