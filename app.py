8cbea84d2bc81a7e840602828f3a8315f9108a9a
William Bell <wjbell97@gmail.com>
2021-04-04 00:07:55 -0400

    Cleaning up app.py

--- app.py
+++ app.py
@@ -19,33 +19,6 @@ from services.steam_service import generate_friends_online_message
 
 app = Flask(__name__)
 
-if __name__ == '__main__':
-    gunicorn_logger = logging.getLogger('gunicorn.error')
-    app.logger.handlers = gunicorn_logger.handlers
-    app.logger.setLevel(gunicorn_logger.level)
-
-# db_name = 'last_game_state.db'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_name
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-
-
-# class RecentGamesDB(db.Model):
-#     match_id = db.Column(db.Integer, primary_key=True)
-#     start_time = db.Column(db.Integer)
-#     side = db.Column(db.String(7))
-#     victory = db.Column(db.Integer)
-#     duration = db.Column(db.Integer)
-#     hero = db.Column(db.String(30))
-#     kills = db.Column(db.Integer)
-#     deaths = db.Column(db.Integer)
-#     assists = db.Column(db.Integer)
-#     with_heroes = db.Column(db.String(150))
-#     with_friends = db.Column(db.String(30))
-#     against_heroes = db.Column(db.String(150))
-
-#     def __repr__(self):
-        # return f'Match {self.match_id}'
-
 
 @app.route('/', methods=['POST'])
 def webhook():
@@ -78,35 +51,3 @@ def webhook():
 def keep_alive_webhook():
     app.logger.debug('Keep alive route received a ping')
     return "ok", 200
-
-
-if __name__ == '__main__':
-    manager = Manager()
-    last_game_state = manager.Namespace()
-    last_game_state.last_query_time = time.time()
-    last_game_state.match_id = -1
-    last_game_state.start_time = -1
-    last_game_state.side = ''
-    last_game_state.victory = 0
-    last_game_state.duration = -1
-    last_game_state.hero = ''
-    last_game_state.kills = -1
-    last_game_state.deaths = -1
-    last_game_state.assists = -1
-    last_game_state.with_heroes = tuple()
-    last_game_state.with_friends = tuple()
-    last_game_state.against_heroes = tuple()
-    last_game_state.houstons_GPM = -1
-    last_game_state.friends_deaths = {}
-
-    p = Process(target=dota_game_service, args=(last_game_state,))
-    p.start()
-
-    value = Value('b', True)
-    p2 = Process(target=keep_alive, args=(value,))
-    p2.start()
-
-    app.run(debug=True, use_reloader=False)
-    
-    p.join()
-    p2.join()
