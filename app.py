fd0a5c7aa85f2062bf684ca7134d786555a9c4cb
William Bell <wjbell97@gmail.com>
2021-04-04 01:26:24 -0400

    left a deprecated import in app.py

--- app.py
+++ app.py
@@ -1,22 +1,14 @@
-import logging
 import time
-from importlib import reload
-from multiprocessing import Manager, Process, Value
 
 from flask import Flask, request
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.sql import text
 
 from common import send_message, send_message_tony
 from interaction.conversation import (generate_excuse, no_prompt, oi_huey,
                                       question_about_friends_online,
                                       question_about_last_game,
                                       request_to_do_something, tony_response)
-from services.dota_game_service import (dota_game_service,
-                                        generate_old_game_notification,
+from services.dota_game_service import (generate_old_game_notification,
                                         get_last_match_data)
-from services.keep_alive_service import keep_alive
-# from services.post_service import post_queue
 from services.steam_services import generate_friends_online_message
 
 app = Flask(__name__)
